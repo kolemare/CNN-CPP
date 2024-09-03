587b785f3e86f26609cdb623f0b6b637728d8c40
kolemare <kolemareee@gmail.com>
2024-09-03 08:30:46 +0200

    CNN-CPP Cifar10 Results

--- src/main.cpp
+++ src/main.cpp
@@ -41,8 +41,8 @@ int main(int argc, char **argv)
             // cnn_cd_elrales_e25();
             // cnn_cd_ld_e25();
             // cnn_cd_nb_e25();
-            cnn_cifar10_e10();
-            // cnn_mnist_e3();
+            // cnn_cifar10_e10();
+            cnn_mnist_e3();
             // cnn_sanity_2_colors();
             // cnn_sanity_2_shapes();
             // cnn_sanity_5_colors();
